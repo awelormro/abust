09feaf965e57c5295d88b1f063f03de8fb3a1984
awelormro <ssuuppeerr@live.com.mx>
2023-07-12 13:54:50 -0600

    update to control tab keys

--- config.def.h
+++ config.def.h
@@ -430,7 +430,7 @@ static Key key[] = {
 	{ XK_Right,         XK_ANY_MOD,     "\033[C",        0,   -1},
 	{ XK_Right,         XK_ANY_MOD,     "\033OC",        0,   +1},
 	{ XK_ISO_Left_Tab,  ShiftMask,      "\033[Z",        0,    0},
-	{ XK_ISO_Left_Tab, ShiftMask|ControlMask, "\033[Z22", 0,   0},
+	{ XK_ISO_Left_Tab, ShiftMask|ControlMask, "\033[{", 0,   0},
 	{ XK_Return,        Mod1Mask,       "\033\r",        0,    0},
 	{ XK_Return,        XK_NO_MOD,      "\r",            0,    0},
 	{ XK_Insert,        ShiftMask,      "\033[4l",      -1,    0},
